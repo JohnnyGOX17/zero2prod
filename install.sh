683d7029c056d1b738f3d24ab387860ab54af6ec
Gentile, John <jcgent@amazon.com>
2023-12-20 22:16:47 -0600

    integ test for HTTP GET

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ echo "> Installing code coverage"
 cargo install cargo-tarpaulin
 echo "> Check crates for security vulnerabilities"
 cargo install --locked cargo-deny && cargo deny init
+echo "> Installing cargo expand"
+cargo install cargo-expand
 echo "> Installing Rust linting and formatter"
 rustup component add clippy rustfmt
 
